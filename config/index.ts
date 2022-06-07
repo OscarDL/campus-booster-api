42aeb1ad5eb21b9db1937bb7410beb56a027836e
Dulysse <292878@supinfo.com>
2022-06-07 17:16:54 +0200

    goooo

--- config/index.ts
+++ config/index.ts
@@ -6,7 +6,7 @@ dotenv.config();
 
 if(fs.existsSync(path.join(process.cwd(), '.env'))) {
   dotenv.config();
-} else {
+} else if(process.env.NODE_ENV !== 'production') {
   console.log(".env file required!");
   process.kill(process.pid);
 }
