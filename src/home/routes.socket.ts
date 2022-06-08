38ee909271f8fb61125c667df6231499c75cbae8
Oscar Di Lenarda <oscar@dilenarda.com>
2022-06-08 23:53:17 +0200

    Implemented home (summary) routes + fixed production CORS logic.

--- src/home/routes.socket.ts
+++
@@ -1,7 +0,0 @@
-import { ISocket } from "../../types/socket";
-import { CampusAttributes } from "./model/home.interface";
-export default (socket: ISocket): void => {
-    socket.on("new-campus", (campus: CampusAttributes): void => {
-        socket.broadcast.emit("new-campus", campus);
-    });
-}
