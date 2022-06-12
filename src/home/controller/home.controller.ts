38a6c390b72d59e0299f30b66c9da3bbed0ebf69
Oscar Di Lenarda <oscar@dilenarda.com>
2022-06-13 00:33:59 +0200

    Added summary upcoming planning for admins.

--- src/home/controller/home.controller.ts
+++ src/home/controller/home.controller.ts
@@ -5,6 +5,7 @@ import * as UserService from '../../users/service/user.service';
 import * as GradeService from '../../grades/service/grade.service';
 import * as CampusService from '../../campus/service/campus.service';
 import * as CourseService from '../../courses/service/course.service';
+import * as ContractService from '../../contracts/service/contract.service';
 import * as PlanningService from '../../plannings/service/planning.service';
 import { UserModel } from '../../users/model/user.interface';
 import boom from '@hapi/boom';
@@ -18,6 +19,7 @@ export type Summary = {
   campus: number,
   courses: number,
   students: number,
+  contracts?: number,
   annualCredits?: number,
   latestGrades?: GradeModel[],
   upcomingCourses?: PlanningModel[]
@@ -35,6 +37,7 @@ function getUserCurrentYear(user: UserModel): number {
 
 export async function getSummary(req: Req, res: Res, next: Next): Promise<Resp> {
     try {
+        const user = req.user;
         const summary: Summary = {
             campus: 0,
             courses: 0,
@@ -51,8 +54,7 @@ export async function getSummary(req: Req, res: Res, next: Next): Promise<Resp> 
             }
         })).length;
 
-        if (req.user && req.user.role === roles.Student) {
-            const user = req.user;
+        if (user && user.role === roles.Student) {
             const grades = await GradeService.findAll({
                 where: {
                     userId: user.id
@@ -69,6 +71,13 @@ export async function getSummary(req: Req, res: Res, next: Next): Promise<Resp> 
             .filter(planning => user.UserHasClassrooms?.map(uhc => uhc.classroomId).includes(planning.ClassroomHasCourse?.classroomId))
             .filter(planning => moment().isSameOrBefore(moment(planning.date), 'day'))
             .sort((a, b) => moment(a.date).diff(moment(b.date))).slice(0, 3);
+        } else if (user) {
+          summary.contracts = (await ContractService.findAll()).length;
+
+          summary.upcomingCourses = (await PlanningService.findAll())
+          .filter(planning => user.campusId ? planning.ClassroomHasCourse?.Classroom?.campusId === user.campusId : true)
+          .filter(planning => moment().isSameOrBefore(moment(planning.date), 'day'))
+          .sort((a, b) => moment(a.date).diff(moment(b.date))).slice(0, 5);
         }
 
         return res.status(200).json(summary);
